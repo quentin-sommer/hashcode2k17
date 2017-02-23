3b28ae68c9e648c9a16630a00dc587663b3d745b
Quentin Sommer <sommerquentin67@gmail.com>
2017-02-23 18:59:48 +0100

    parsed initial line

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import {readLinePerLineSync} from './utils'
 
-const filename = './exampleData.csv'
+const filename = './input/me_at_the_zoo.in'
 const dataStore = {}
 
 const processLine = (line, lineNumber) => {
-  console.log(lineNumber)
+  switch (lineNumber) {
+    case 1 :
+      const arr = line.split(' ')
+      dataStore.videoCount = arr[0]
+      dataStore.endpointsCount = arr[1]
+      dataStore.requestsCount = arr[2]
+      dataStore.cacheServCount = arr[3]
+      dataStore.cacheServCapacity = arr[4]
+      break
+  }
   dataStore.lines.push(line.split(','))
 }
 
 const doSomethingElse = () => {
-  console.log('doSomethingElse')
 }
 
 const main = () => {
   dataStore.lines = []
   readLinePerLineSync(filename, processLine)
-    .then(console.log)
     .then(doSomethingElse)
-    .then(() => console.log(dataStore.lines))
 }
 
 main()
